d2c420644da979a16813e75900f8fd3f64f156fd
santyjL <jlopezsanty1@gmail.com>
2024-01-02 21:15:15 -0600

    Update Hello_git_print.py
    se añadieron las librerias os y time para una interfaz mas bonita he interactiva

--- Hello_git_print.py
+++ Hello_git_print.py
@@ -1,3 +1,6 @@
+import os
+import time
+
 from Hello_git import color_text, lenguaje_hacker
 from opciones_replace import numeros_romanos, reverso
 
@@ -15,18 +18,28 @@ class LenguajeElegido:
         self.original = original
 
     def presenteacion(self):
+        os.system("cls")
         print(MAGENTA)
         print("/|-\|/|-\|/|-\|/|-\|/|-\|/|-\|/|-\|/|-\|/|-\|/|-\|/|-\|")
+        time.sleep(0.2)
 
         print(self.color)
         print(f"valor introducido en el lenguaje : {self.nombre}:")
+        time.sleep(0.2)
         print(self.traducion)
+        time.sleep(0.2)
+
 
         print(MAGENTA)
         print("/|-\|/|-\|/|-\|/|-\|/|-\|/|-\|/|-\|/|-\|/|-\|/|-\|/|-\|")
+        time.sleep(0.2)
         print(self.color)
         print("texto original : ")
+        time.sleep(0.2)
         print(self.original)
+
+        input("precione enter para seguir : ")
+        os.system("cls")
 
 
 #presenteacion
@@ -45,9 +58,13 @@ while True:
                 3.decimal_a_romano""")
 
     lenguaje = int(input("introduce el lenguaje a utilizar segun el indice : "))
+    os.system("cls")
+    time.sleep(0.2)
 
     if lenguaje == 1 or lenguaje == 2:
         texto : str = input("introduce el texto a traducir : ")
+        os.system("cls")
+        time.sleep(0.2)
 
         if lenguaje == 1:
             traducion , color = lenguaje_hacker(texto)
@@ -61,14 +78,22 @@ while True:
 
     elif lenguaje == 3:
         numero = int(input("introduce el numero a combertir : "))
+        os.system("cls")
+        time.sleep(0.2)
 
         combercion , color = numeros_romanos.decimal_a_romano(numero)
         llamado = LenguajeElegido("natural a romano" ,color , combercion , numero)
         llamado.presenteacion()
 
     else :
+        os.system("cls")
+        time.sleep(0.2)
         print("esa no es una opcion a elegir")
+        input("precione enter para continuar : ")
 
+    print(MAGENTA)
+    print("/|-\|/|-\|/|-\|/|-\|/|-\|/|-\|/|-\|/|-\|/|-\|/|-\|/|-\|\n")
+    print(color)
     seguir =  input("va a seguir usando el traductor , yes/no : ")
 
     if seguir.lower() == "yes":
